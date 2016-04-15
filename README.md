98543a2111eb47a04cb4cc090ee955e30dad63af
Fumikazu Fujiwara <github@ze.gs>
2016-04-16 06:42:59 +0900

    line -> facebook

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# line-ichibaitemsearch
+# facebook-ichibaitemsearch
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
 
-LINE BOT with Rakuten Ichiba Item Search
-
-![image](https://qr-official.line.me/sid/L/aue0852b.png)
+Facebook BOT with Rakuten Ichiba Item Search
--- index.php
+++ index.php
@@ -8,56 +8,63 @@ $app = new \Slim\App;
 $app->post('/', function (Request $request, Response $response) {
     // message from LINE server
     $body = json_decode($request->getBody(), true);
+    error_log(__FILE__.":".__LINE__.":".print_r($body,true);
 
-    foreach ($body['result'] as $msg) {
-        //Search by Rakuten Web Service
-        $rwsClient = new RakutenRws_Client();
-        $rwsClient-> setApplicationId(getenv('RAKUTEN_WEBSERVICE_APPLICATIN_ID'));
-        $rwsClient-> setAffiliateId(getenv('RAKUTEN_WEBSERVICE_AFFILIATE_ID'));
-        $rwsResponse = $rwsClient->execute('IchibaItemSearch', array(
-            'keyword' => $msg['content']['text'], // from message text
-            'hits'    => 3,                       // #of Items
-            'carrier' => 2                        // for smart phone
-        ));
-        if (!$rwsResponse->isOk()) {
-            error_log(__FILE__.":".__LINE__.":".$rwsResponse->getMessage());
-            continue;
-        }
+    foreach ($body['entry'] as $entry) {
+        foreach ($entry['messaging'] as $msg) {
+            //Search by Rakuten Web Service
+            $rwsClient = new RakutenRws_Client();
+            $rwsClient-> setApplicationId(getenv('RAKUTEN_WEBSERVICE_APPLICATIN_ID'));
+            $rwsClient-> setAffiliateId(getenv('RAKUTEN_WEBSERVICE_AFFILIATE_ID'));
+            $rwsResponse = $rwsClient->execute('IchibaItemSearch', array(
+                'keyword' => $msg['content']['text'], // from message text
+                'hits'    => 3,                       // #of Items
+                'carrier' => 2                        // for smart phone
+            ));
+            if (!$rwsResponse->isOk()) {
+                error_log(__FILE__.":".__LINE__.":".$rwsResponse->getMessage());
+                continue;
+            }
 
-        //Respond message
-        foreach ($rwsResponse['Items'] as $item) {
-            $resContent = $msg['content'];
-            $resContent['text'] = "";
-            $resContent['text'] .= $item['Item']['itemName']."\n";
-            $resContent['text'] .= $item['Item']['itemUrl'];
+            //Respond message
+            foreach ($rwsResponse['Items'] as $item) {
+                $resContent = $msg['content'];
+                $resContent['text'] = "";
+                $resContent['text'] .= $item['Item']['itemName']."\n";
+                $resContent['text'] .= $item['Item']['itemUrl'];
 
-            $requestOptions = [
-                'body' => json_encode([
-                    'to'        => [$msg['content']['from']],
-                    'toChannel' => 1383378250,           // Fixed value
-                    'eventType' => '138311608800106203', // Fixed value
-                    'content'   => $resContent,
-                ]),
-                'headers' => [
-                    'Content-Type'                 => 'application/json; charset=UTF-8',
-                    'X-Line-ChannelID'             => getenv('LINE_CHANNEL_ID'),
-                    'X-Line-ChannelSecret'         => getenv('LINE_CHANNEL_SECRET'),
-                    'X-Line-Trusted-User-With-ACL' => getenv('LINE_CHANNEL_MID'),
-                ],
-                'proxy' => [
-                    'https' => getenv('FIXIE_URL'),
+                $requestOptions = [
+                    'body' => json_encode([
+                        'recipient' => ['id' => [$msg['sender']['id']]],
+                        'message'   => ['text' => $resContent]
+                    ]),
+                    'headers' => [
+                        'Content-Type'                 => 'application/json'
                     ]
                 ];
 
-            $client = new GuzzleHttp\Client();
-            try {
-                $client->request('post', 'https://trialbot-api.line.me/v1/events', $requestOptions);
-            } catch (Exception $e) {
-                error_log(__FILE__.":".__LINE__.":".$e->getMessage());
+                $client = new GuzzleHttp\Client();
+                try {
+                    $client->request('post', 
+                        'https://graph.facebook.com/v2.6/me/messages?access_token='.getenv('FACEBOOK_ACCES_TOKEN'), 
+                        $requestOptions);
+                } catch (Exception $e) {
+                    error_log(__FILE__.":".__LINE__.":".$e->getMessage());
+                }
             }
         }
     }
 
     return $response;
 });
+$app->get('/', function (Request $request, Response $response) {
+    $query = $request -> getQueryParams();
+    error_log(__FILE__.":".__LINE__.":".print_r($query,true);
+    if ($query['hub.verify_token'] == getenv('FACEBOOK_VALIDATION_TOKEN')) {
+        error_log(__FILE__.":".__LINE__);
+        $response->getBody()->write($query['hub.challenge']);
+    }
+    error_log(__FILE__.":".__LINE__);
+    $response->getBody()->write('Error, wrong validation token');
+}
 $app->run();
